24c44c9068ad82e6b67f1c6b8d08563d8ef70e0e
John Arundel <john@bitfieldconsulting.com>
2019-04-23 20:26:52 +0100

    Reimplement Match and Reject to use EachLine

--- filters.go
+++ filters.go
@@ -9,49 +9,29 @@ import (
 // contain the specified string. If there is an error reading the pipe, the
 // pipe's error status is also set.
 func (p *Pipe) Match(s string) *Pipe {
-	if p.Error() != nil {
-		return p
-	}
-	scanner := bufio.NewScanner(p.Reader)
-	output := strings.Builder{}
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), s) {
-			output.WriteString(scanner.Text())
-			output.WriteByte('\n')
+	return p.EachLine(func(line string, out *strings.Builder) {
+		if strings.Contains(line, s) {
+			out.WriteString(line)
+			out.WriteByte('\n')
 		}
-	}
-	err := scanner.Err()
-	if err != nil {
-		p.SetError(err)
-	}
-	p.Close()
-	return Echo(output.String())
+	})
 }
 
 // Reject reads from the pipe, and returns a new pipe containing only lines
 // which do not contain the specified string. If there is an error reading the
 // pipe, the pipe's error status is also set.
 func (p *Pipe) Reject(s string) *Pipe {
-	if p.Error() != nil {
-		return p
-	}
-	scanner := bufio.NewScanner(p.Reader)
-	output := strings.Builder{}
-	for scanner.Scan() {
-		if !strings.Contains(scanner.Text(), s) {
-			output.WriteString(scanner.Text())
-			output.WriteByte('\n')
+	return p.EachLine(func(line string, out *strings.Builder) {
+		if !strings.Contains(line, s) {
+			out.WriteString(line)
+			out.WriteByte('\n')
 		}
-	}
-	err := scanner.Err()
-	if err != nil {
-		p.SetError(err)
-	}
-	p.Close()
-	return Echo(output.String())
+	})
 }
 
-// EachLine calls the specified function once for each line of the input. Anything the function returns
+// EachLine calls the specified function for each line of input, passing it the
+// line as a string, and a *strings.Builder to write its output to. The return
+// value from EachLine is a pipe containing the contents of the strings.Builder.
 func (p *Pipe) EachLine(process func(string, *strings.Builder)) *Pipe {
 	if p.Error() != nil {
 		return p
