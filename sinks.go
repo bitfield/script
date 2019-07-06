c93ea379a697351d12a76bbdc897babf79307d99
John Arundel <john@bitfieldconsulting.com>
2019-07-06 17:43:44 +0100

    Alphabetise sinks, sources, and pipes

--- sinks.go
+++ sinks.go
@@ -8,16 +8,12 @@ import (
 	"strings"
 )
 
-// String returns the contents of the Pipe as a string, or an error, and closes
-// the pipe after reading. If there is an error reading, the pipe's error status
+// AppendFile appends the contents of the Pipe to the specified file, and closes
+// the pipe after reading. It returns the number of bytes successfully written,
+// or an error. If there is an error reading or writing, the pipe's error status
 // is also set.
-func (p *Pipe) String() (string, error) {
-	data, err := p.Bytes()
-	if err != nil {
-		p.SetError(err)
-		return "", err
-	}
-	return string(data), nil
+func (p *Pipe) AppendFile(fileName string) (int64, error) {
+	return p.writeOrAppendFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY)
 }
 
 // Bytes returns the contents of the Pipe as a slice of byte, or an error. If
@@ -48,20 +44,39 @@ func (p *Pipe) CountLines() (int, error) {
 	return lines, p.Error()
 }
 
+// Stdout writes the contents of the pipe to the program's standard output. It
+// returns the number of bytes successfully written, plus a non-nil error if the
+// write failed or if there was an error reading from the pipe. If the pipe has
+// error status, Stdout returns zero plus the existing error.
+func (p *Pipe) Stdout() (int, error) {
+	if p == nil || p.Error() != nil {
+		return 0, p.Error()
+	}
+	output, err := p.String()
+	if err != nil {
+		return 0, err
+	}
+	return fmt.Print(output)
+}
+
+// String returns the contents of the Pipe as a string, or an error, and closes
+// the pipe after reading. If there is an error reading, the pipe's error status
+// is also set.
+func (p *Pipe) String() (string, error) {
+	data, err := p.Bytes()
+	if err != nil {
+		p.SetError(err)
+		return "", err
+	}
+	return string(data), nil
+}
+
 // WriteFile writes the contents of the Pipe to the specified file, and closes
 // the pipe after reading. It returns the number of bytes successfully written,
 // or an error. If there is an error reading or writing, the pipe's error status
 // is also set.
 func (p *Pipe) WriteFile(fileName string) (int64, error) {
 	return p.writeOrAppendFile(fileName, os.O_RDWR|os.O_CREATE)
-}
-
-// AppendFile appends the contents of the Pipe to the specified file, and closes
-// the pipe after reading. It returns the number of bytes successfully written,
-// or an error. If there is an error reading or writing, the pipe's error status
-// is also set.
-func (p *Pipe) AppendFile(fileName string) (int64, error) {
-	return p.writeOrAppendFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY)
 }
 
 func (p *Pipe) writeOrAppendFile(fileName string, mode int) (int64, error) {
@@ -80,19 +95,4 @@ func (p *Pipe) writeOrAppendFile(fileName string, mode int) (int64, error) {
 		return 0, err
 	}
 	return wrote, nil
-}
-
-// Stdout writes the contents of the pipe to the program's standard output. It
-// returns the number of bytes successfully written, plus a non-nil error if the
-// write failed or if there was an error reading from the pipe. If the pipe has
-// error status, Stdout returns zero plus the existing error.
-func (p *Pipe) Stdout() (int, error) {
-	if p == nil || p.Error() != nil {
-		return 0, p.Error()
-	}
-	output, err := p.String()
-	if err != nil {
-		return 0, err
-	}
-	return fmt.Print(output)
 }
