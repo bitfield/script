590add32a55779578efc04edb23be80897c8a6d8
Thomas Poignant <thomas.poignant@adevinta.com>
2020-06-30 15:47:27 +0200

    Add comment to the example for slice

--- examples/slice/main.go
+++ examples/slice/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
+	// Read the first args, and put the 10 first lines which match the word Error.
 	errors, err := script.Args().Concat().Match("Error").First(10).Slice()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Printing the errors
 	for _, e := range errors {
 		fmt.Printf("ERROR: %s", e)
 	}
