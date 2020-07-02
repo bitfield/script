dcca15270c4e431e7147696e396e313c41728695
Thomas Poignant <thomas.poignant@adevinta.com>
2020-07-02 09:35:05 +0200

    Add example file

--- examples/slice/main.go
+++ examples/slice/main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	// Read the first args, and put the 10 first lines which match the word Error.
-	errors, err := script.Args().Concat().Match("Error").First(10).Slice()
+	errors, err := script.File("./examples/slice/example.txt").Match("error").First(10).Slice()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Printing the errors
 	for _, e := range errors {
-		fmt.Printf("ERROR: %s", e)
+		fmt.Printf("ERROR: %s\n", e)
 	}
 }
