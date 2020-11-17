79e8e1419eb748e463dec871a5738827b1ed5140
Latiif <abdullatif.alshriaf@combitech.se>
2020-11-17 21:01:24 +0100

    Add example for Prompt

---
+++ examples/prompt/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/bitfield/script"
+)
+
+func main() {
+	script.Echo("Installation of Fake Product\n").Stdout()
+	curDir, err := script.Exec("pwd").String()
+	if err != nil {
+		panic(err)
+	}
+
+	script.Prompt("Choose install location", strings.TrimSpace(curDir)).Stdout()
+
+	script.Echo("Installation of Fake Product complete").Stdout()
+}
