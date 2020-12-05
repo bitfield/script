1155d7400562091d3992466e676ffb023c6ef994
Latiif <latiif@mail.com>
2020-12-05 15:17:43 +0100

    Use `readInput` in prompt example

--- examples/prompt/main.go
+++ examples/prompt/main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/bitfield/script"
 )
 
+func Install(path string) {
+	script.Echo("📂 Installing at " + path + "\n").Stdout()
+	script.Echo("🟢 Successfully installed\n").Stdout()
+}
+
 func main() {
 	script.Echo("Installation of Fake Product\n").Stdout()
 	curDir, err := script.Exec("pwd").String()
@@ -13,7 +18,6 @@ func main() {
 		panic(err)
 	}
 
-	script.Prompt("Choose install location", strings.TrimSpace(curDir)).Stdout()
-
-	script.Echo("Installation of Fake Product complete").Stdout()
+	path, _ := script.Echo("Choose install location").ReadInput(strings.TrimSpace(curDir)).String()
+	Install(path)
 }
