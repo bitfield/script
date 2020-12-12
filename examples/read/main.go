cb623bdf5a9c03ce55762aa32f5527db62e65534
Latiif <latiif@mail.com>
2020-12-12 13:25:29 +0100

    Use output pipe from ReadInput in 'read' example

--- examples/read/main.go
+++ examples/read/main.go
@@ -6,11 +6,6 @@ import (
 	"github.com/bitfield/script"
 )
 
-func Install(path string) {
-	script.Echo("📂 Installing at " + path + "\n").Stdout()
-	script.Echo("🟢 Successfully installed\n").Stdout()
-}
-
 func main() {
 	script.Echo("Installation of Fake Product\n").Stdout()
 	curDir, err := script.Exec("pwd").String()
@@ -18,6 +13,5 @@ func main() {
 		panic(err)
 	}
 
-	path, _ := script.Echo("Choose install location").ReadInput(strings.TrimSpace(curDir)).String()
-	Install(path)
+	script.Echo("Choose install location").ReadInput(strings.TrimSpace(curDir)).ExecForEach("echo Installing at {{.}}").Stdout()
 }
