40add6d6e3b1e090f0e9be8256d0df86627303b9
John Arundel <john@bitfieldconsulting.com>
2026-06-03 11:26:54 +0100

    new para for `deprecated` comments

--- script.go
+++ script.go
@@ -871,6 +871,7 @@ func (p *Pipe) SetError(err error) {
 
 // SHA256Sum returns the hex-encoded SHA-256 hash of the entire contents of the
 // pipe, or an error.
+// 
 // Deprecated: SHA256Sum has been deprecated by [Pipe.Hash]. To get the SHA-256
 // hash for the contents of the pipe, call `Hash(sha256.new())`
 func (p *Pipe) SHA256Sum() (string, error) {
@@ -880,6 +881,7 @@ func (p *Pipe) SHA256Sum() (string, error) {
 // SHA256Sums reads paths from the pipe, one per line, and produces the
 // hex-encoded SHA-256 hash of each corresponding file, one per line. Any files
 // that cannot be opened or read will be ignored.
+// 
 // Deprecated: SHA256Sums has been deprecated by [Pipe.HashSums]. To get the SHA-256
 // hash for each file path in the pipe, call `HashSums(sha256.new())`
 func (p *Pipe) SHA256Sums() *Pipe {
